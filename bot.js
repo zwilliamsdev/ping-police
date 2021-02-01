cda2982b6ec3c916290b0ac37c927bb9ce8e2619
Zach Williams <zwilliams.dev@gmail.com>
2021-01-31 22:15:37 -0500

    created a strings file and refactored hardcoded strings

--- bot.js
+++ bot.js
@@ -11,6 +11,7 @@ const Client = new Discord.Client();
 
 // Configuration
 require('./config')
+require('./strings')
 require('dotenv').config()
 
 Client.on('ready', () => {
@@ -31,17 +32,17 @@ Client.on('message', message => {
                     .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({
                         dynamic: true
                     }))
-                    .setDescription(`Message sent by ${message.author} has been \`Deleted\` by <@712450623416565833>`)
-                    .addField(`Reason:`, '\`Pinging Owner!\`')
+                    .setDescription(`Message sent by ${message.author} has been \`Deleted\` by ${Strings.name}`)
+                    .addField(`Reason:`, Strings.admin.reason)
                     .setColor('#e52030')
                 Client.channels.cache.get(Config.reporting).send(embed)
                 message.delete().catch(console.error)
 
                 // Ping Embed DM
                 const pingdm = new MessageEmbed()
-                    .setAuthor(`Anti Ping | ${message.guild.name}`, `https://cdn.discordapp.com/icons/756182992304734251/a_72446774f59bb077e1b70b6ab9fd1312.webp?size=256`)
-                    .setDescription(`You have received a warning for **Pinging Emilie**.`)
-                    .addField('Rule Violated:', `\`Rule 1 -\` Do not directly use the \`@\` function to ping Emilie to get her attention, you can write her name in your messages but not ping her in channels.`)
+                    .setAuthor(`${Strings.name} | ${message.guild.name}`, `https://cdn.discordapp.com/icons/756182992304734251/a_72446774f59bb077e1b70b6ab9fd1312.webp?size=256`)
+                    .setDescription(Strings.dm.desc)
+                    .addField('Rule Violated:', Strings.dm.rule)
                     .addField('Rule Violater:', `\`${message.author.tag}\``)
                     .setColor('#e52030')
                 message.author.send(pingdm)
@@ -49,7 +50,7 @@ Client.on('message', message => {
 
                 //ping Embed Server
                 const pingserver = new MessageEmbed()
-                    .setAuthor(`Please do not ping Emilie, ${message.author.tag}`, `https://cdn.discordapp.com/emojis/804388367126626324.png?v=1`)
+                    .setAuthor(`Please do not ping Emilie, ${message.author.tag} you can just type her name and she will see it.`, `https://cdn.discordapp.com/emojis/804388367126626324.png?v=1`)
                     .setColor('#e52030')
                 message.channel.send(pingserver).then(msg => msg.delete({
                     timeout: 15000
---
+++ strings.js
@@ -0,0 +1,10 @@
+Strings = {
+    "name": "Ping Police",
+    "admin": {
+        "reason": "`Rule #1: Pinging Pilot Emilie`"
+    },
+    "dm": {
+        "desc": "If you just type Emilies name she will see your message in the Discord.",
+        "rule": "**1** - Do not directly use the `@` function to ping / tag Emilie. You can just type her name normally."
+    }
+}
